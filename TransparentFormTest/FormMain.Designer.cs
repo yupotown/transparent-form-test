6d4930738135362fba5d8c19466d6679eaeb6a48
Yupo <yupotown@gmail.com>
2015-11-30 11:36:05 +0900

    画面の右下に合わせて表示するようにした

--- TransparentFormTest/FormMain.Designer.cs
+++ TransparentFormTest/FormMain.Designer.cs
@@ -28,7 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.buttonClose = new System.Windows.Forms.Button();
+            this.labelClock = new System.Windows.Forms.Label();
+            this.timerSecond = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // buttonClose
@@ -41,23 +44,43 @@
             this.buttonClose.UseVisualStyleBackColor = true;
             this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
             // 
+            // labelClock
+            // 
+            this.labelClock.AutoSize = true;
+            this.labelClock.BackColor = System.Drawing.Color.Transparent;
+            this.labelClock.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.labelClock.Location = new System.Drawing.Point(192, 219);
+            this.labelClock.Name = "labelClock";
+            this.labelClock.Size = new System.Drawing.Size(0, 12);
+            this.labelClock.TabIndex = 1;
+            // 
+            // timerSecond
+            // 
+            this.timerSecond.Enabled = true;
+            this.timerSecond.Interval = 1000;
+            this.timerSecond.Tick += new System.EventHandler(this.timerSecond_Tick);
+            // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(320, 240);
+            this.Controls.Add(this.labelClock);
             this.Controls.Add(this.buttonClose);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FormMain";
             this.Text = "TransparentFormTest";
             this.Load += new System.EventHandler(this.FormMain_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.Label labelClock;
+        private System.Windows.Forms.Timer timerSecond;
     }
 }
 
--- TransparentFormTest/FormMain.cs
+++ TransparentFormTest/FormMain.cs
@@ -25,11 +25,20 @@ namespace TransparentFormTest
             this.BackgroundImage = bmp;
             this.BackColor = transKey;
             this.TransparencyKey = transKey;
+
+            var screenW = Screen.PrimaryScreen.WorkingArea.Width;
+            var screenH = Screen.PrimaryScreen.WorkingArea.Height;
+            this.SetDesktopLocation(screenW - this.Width, screenH - this.Height);
         }
 
         private void buttonClose_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void timerSecond_Tick(object sender, EventArgs e)
+        {
+            labelClock.Text = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");
         }
     }
 }
